5f77feee76f7a7c312a67f962716ae9e796128a9
Viktoria <vik25@i.ua>
2023-01-11 12:43:14 +0100

    mark2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -28,6 +28,7 @@ const FEEDBACK_FORM = "feedback-form-state";
 const formData = {};
 
 const handleInput = (event) => {
+   
     formData[event.target.name] = event.target.value;
     localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formData));
     
